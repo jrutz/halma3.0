d2dd76d2aac8e096c86632ee2c68a6a7e48e0268
Derek Phanekham <dphanekham@smu.edu>
2014-12-05 02:10:33 -0600

    Added make json functionality to remoteai.cpp

--- HalmaImplementation/remoteai.cpp
+++ HalmaImplementation/remoteai.cpp
@@ -6,6 +6,7 @@ RemoteAI::RemoteAI(QString url = "") {
 
     sampleAI_2 = "http://lyle.smu.edu/~coyle/halmagame/code/makeOneMove.php";
     sampleAI = "http://lyle.smu.edu/~tbgeorge/cse4345/a1/getMove.php";
+    derekAI = "http://lyle.smu.edu/~dphanekham/4345/HW11.php";
 
     sampleJsonString =  QString::fromStdString("{\"pieces\":[{\"y\":14,\"x\":0,\"team\":0},{\"y\":15,\"x\":0,\"team\":0},")
     + QString::fromStdString( "{\"y\":16,\"x\":0,\"team\":0},{\"y\":17,\"x\":0,\"team\":0},{\"y\":14,\"x\":1,\"team\":0},")
@@ -31,11 +32,65 @@ void RemoteAI::SetConnectionURL(QString url) {
     connectionUrl = url;
 }
 
-bool RemoteAI::testConnectionURL(QString url) {
+bool RemoteAI::TestConnectionURL(QString url) {
     return false;
 }
 
+/*
+
+*/
+QJsonObject RemoteAI::MakeJsonObject(int numPieces,int destSize, int piecesX[], int piecesY[], int destX[],
+                                     int destY[], int enemyX[], int enemyY[],
+                                     int enemyDestX[], int enemyDestY[]) {
+    QJsonObject JsonObject;
+    QJsonArray piecesJsonArray;
+    QJsonArray enemyJsonArray;
+    QJsonArray destJsonArray;
+    QJsonArray enemyDestJsonArray;
+
+    for(int i=0; i< numPieces; i++) {
+        QJsonObject temp;
+        QJsonObject temp2;
+        temp.insert("x", piecesX[i]);
+        temp.insert("y", piecesY[i]);
+        temp2.insert("x", enemyX[i]);
+        temp2.insert("y", enemyY[i]);
+
+        piecesJsonArray.append(temp);
+        enemyJsonArray.append(temp2);
+    }
+
+    for(int i=0; i< destSize; i++) {
+        QJsonObject temp;
+        QJsonObject temp2;
+        temp.insert("x", destX[i]);
+        temp.insert("y", destY[i]);
+        temp2.insert("x", enemyDestX[i]);
+        temp2.insert("y", enemyDestY[i]);
+
+        destJsonArray.append(temp);
+        enemyDestJsonArray.append(temp2);
+    }
+
+    JsonObject.insert("boardSize", 18);
+    JsonObject.insert("pieces", piecesJsonArray);
+    JsonObject.insert("destinations", destJsonArray);
+    JsonObject.insert("enemy", enemyJsonArray);
+    JsonObject.insert("enemydestinations", enemyDestJsonArray);
+    return JsonObject;
+}
+
 QByteArray RemoteAI::PostJsonData(QByteArray data) {
+
+
+    int px [3] = { 1, 1, 1};
+    int py [3] = { 1, 1, 1};
+    QJsonObject JsonObject = MakeJsonObject(3,3,px,py,px,py,px,py,px,py);
+    qDebug() << "JsonObject" << JsonObject["pieces"].toArray().at(0).toObject()["x"].toInt();
+    QJsonDocument doc;
+    doc.setObject(JsonObject);
+    QByteArray j = doc.toJson();
+    //qDebug() << j;
     // create custom temporary event loop on stack
         QEventLoop eventLoop;
 
@@ -45,10 +100,10 @@ QByteArray RemoteAI::PostJsonData(QByteArray data) {
 
         // the HTTP request
         //url needs to be cstring
-        QNetworkRequest req( QUrl( QString::fromStdString(sampleAI)) );
+        QNetworkRequest req( QUrl( QString::fromStdString(derekAI)) );
         req.setRawHeader("Content-Type", "application/json");
         //QNetworkReply *reply = mgr.get(req);
-        QNetworkReply *reply = mgr.post(req,sampleJsonString.toLatin1());
+        QNetworkReply *reply = mgr.post(req, j);
         eventLoop.exec(); // blocks stack until "finished()" has been called
 
         if (reply->error() == QNetworkReply::NoError) {
@@ -59,12 +114,6 @@ QByteArray RemoteAI::PostJsonData(QByteArray data) {
             qDebug() << "Success" << strReply;
             delete reply;
             QJsonDocument jsonResponse = QJsonDocument::fromJson(strReply.toUtf8());
-
-            QJsonObject newObject;
-            QJsonObject newObject1;
-            newObject1.insert("x", 3);
-            newObject.insert("from", newObject1);
-            qDebug() << "newobject:" << newObject["from"].toObject()["x"].toInt();
 
             QJsonObject jsonObj = jsonResponse.object();
 
